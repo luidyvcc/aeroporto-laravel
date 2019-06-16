6f163107475fb9c5d4e8d1cb858c60e71aae8ffb
Luidy Vaccaro <ldy80@outlook.com>
2019-06-16 20:48:15 -0300

    aula 10.09 finalizada

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('site.layouts.app')
+
+@section('content-site')
+
+<div class="content">
+
+   <div class="content text-center">
+        <img src="{{ url('assets/site/images/error-404.png') }}" alt="404">
+   </div>
+
+@endsection
